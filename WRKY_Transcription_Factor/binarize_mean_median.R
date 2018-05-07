204d23de045f3896dd9d8d5410c84bbdddbbbbdb
Aditya Lahiri <31457525+adilahiri@users.noreply.github.com>
2018-05-07 14:11:57 -0500

    compute means and median

---
+++ WRKY_Transcription_Factor/binarize_mean_median.R
@@ -0,0 +1,12 @@
+binarize_mean_median <- function (Mat_Data){
+  means<-apply(Mat_Data,2,mean)
+  
+  for(iter in 1:ncol(Mat_Data)){
+    
+    pos0<- which(Mat_Data[,iter]<= means[iter])
+    pos1<- which(Mat_Data[,iter]> means[iter])
+    Mat_Data[pos0,iter] <-0
+    Mat_Data[pos1,iter] <-1
+  }
+    return(Mat_Data)
+}
