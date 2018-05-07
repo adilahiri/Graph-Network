f958c040ff7f6c7e925cdbc149d610002307bb90
Aditya Lahiri <31457525+adilahiri@users.noreply.github.com>
2018-05-06 23:56:35 -0500

    find the undirected graph

--- MAPK/find_connections.R
+++ MAPK/find_connections.R
@@ -1,5 +1,6 @@
+# Find the adjacent nodes of a given node 
 find_connections <- function (Node_Number,row_data){
-   Parent_Nodes <- which(row_data==1)
+   Parent_Nodes <- which(row_data==1)  
    Position_NodeNum <- which(Parent_Nodes==Node_Number)
    
    if(length(Position_NodeNum>0)){
